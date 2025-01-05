f54a9c7ac6ce7f0d293c9ba95e7003fcdd89fdf6
Brincoveanu Tiberiu <brincoveanutiberiu@gmail.com>
2025-01-05 12:14:29 +0200

    player movement

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
                 return
         screen.fill((000, 000, 000))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
